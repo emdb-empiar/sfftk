fc6ddb140a1916fbfbfc901ffe38a21771a5185b
Paul K. Korir, PhD <pkorir@ebi.ac.uk>
2018-02-28 13:22:05 +0000

    Dependency change
    - fixed h5py required to v2.6 (2.7 has an ugly deprecation warning)

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "sfftk",
-    version = "0.1.1.dev0",
-    packages = find_packages(),
-    author = "Paul K. Korir, PhD",
-    author_email = "pkorir@ebi.ac.uk, paul.korir@gmail.com",
-    description = "Toolkit for working with EMDB-SFF and other segmentation file formats",
-    url = "http://sfftk.readthedocs.io/en/latest/index.html",
-    license = "Apache License",
-    keywords = "EMDB-SFF, SFF, segmentation",
-    install_requires= ["ahds", "lxml", "h5py", "requests", "scikit-image", "bitarray", "numpy-stl", "configparser"],
-    classifiers = [
+    name="sfftk",
+    version="0.1.1.dev0",
+    packages=find_packages(),
+    author="Paul K. Korir, PhD",
+    author_email="pkorir@ebi.ac.uk, paul.korir@gmail.com",
+    description="Toolkit for working with EMDB-SFF and other segmentation file formats",
+    url="http://sfftk.readthedocs.io/en/latest/index.html",
+    license="Apache License",
+    keywords="EMDB-SFF, SFF, segmentation",
+    install_requires=["ahds", "lxml", "h5py==2.6", "requests", "scikit-image", "bitarray", "numpy-stl", "configparser"],
+    classifiers=[
         # maturity
         'Development Status :: 2 - Pre-Alpha',
         # environment
