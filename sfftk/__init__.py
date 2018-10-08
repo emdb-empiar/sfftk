4c778421c0b864113064e820279680efee8fcccc
Paul K. Korir, PhD <pkorir@ebi.ac.uk>
2018-10-08 12:12:46 +0100

    Bugfix
    - try to fix broken Travis/Gitlab CI
    - bug: unable to build due to imports (for h5py which may not be installed)

--- sfftk/__init__.py
+++ sfftk/__init__.py
@@ -6,6 +6,13 @@ BASE_DIR = os.path.dirname(__file__)
 SFFTK_VERSION = 'v0.3.1.dev6'
 
 # get the schema version
-from sfftk.schema import SFFSegmentation
-seg = SFFSegmentation()
-EMDB_SFF_VERSION = seg.version
+"""
+We add sfftk/schema to the library path to prevent running __init__ on import.
+The long-term solution is to move schema/__init__.py to schema/api.py
+"""
+import sys
+sys.path.append('sfftk/schema')
+from emdb_sff import segmentation
+__seg = segmentation()
+
+EMDB_SFF_VERSION = __seg.schemaVersion
